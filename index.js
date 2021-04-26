4d5ab8c794b812076fb55411d4cb3b1d385d4e79
sbalayan1 <balayans2014@yahoo.com>
2021-04-25 21:01:53 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const events = 'surprise';
+let thankYouCards = [];
+
+function writeCards(names,events) {
+    for (let x = 0; x < names.length; x++) {
+        
+        thankYouCards.push(`Thank you, ${names[x]}, for the wonderful ${events} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(x){
+    while (x>=0) {
+        console.log(x--);
+    }
+}
+
