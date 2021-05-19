e6b99665ffc97bd0e62d0e211402995c395e74af
sbalayan1 <balayans2014@yahoo.com>
2021-05-19 12:48:20 -0700

    commit tree baa0aff6fb87f2089af7fda8f424c0fa0926aa21

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// Code your solutions in this file
+//Practice
+// for loop structure is below 
+// for ([initialization]; [condition]; [iteration]) {[loop body]}
 const names = ['Lisa', 'Kaitlin', 'Jan'];
 const events = 'surprise';
+
+
+//important to note that you need an empty array to push the message into the array. 
+//also important to note that you NEED to return the array to change the thankYouCards array. 
+//lastly ${names[x]} is done so that you can iterate over each element in the array vs. pulling the entire array. 
 let thankYouCards = [];
 
-function writeCards(names,events) {
-    for (let x = 0; x < names.length; x++) {
-        
-        thankYouCards.push(`Thank you, ${names[x]}, for the wonderful ${events} gift!`);
+
+function writeCards(names, event){
+    for(let x = 0; x < names.length; x++) {
+        thankYouCards.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
     }
-    return thankYouCards;
+    return thankYouCards
 }
+
+
 
 function countDown(x){
     while (x>=0) {
